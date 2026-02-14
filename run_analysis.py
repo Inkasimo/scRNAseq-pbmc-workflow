346bb4a9d15d6fbdd0dafc72a0bf661b4c77f9e1
inkasimo <inkasimo@gmail.com>
2026-02-14 20:26:20 +0100

    Version 1.0.4

--- run_analysis.py
+++ run_analysis.py
@@ -190,7 +190,7 @@ def main() -> int:
         #sp.add_argument("--image",
             #default="ghcr.io/inkasimo/scrnaseq-pbmc-workflow@sha256:80354b76e76405636c43e73902236e0399d26978a214227afbafa46fc0555bb8")
         sp.add_argument("--image",
-            default="ghcr.io/inkasimo/scrnaseq-pbmc-workflow:v1.0.3-rc2")
+            default="ghcr.io/inkasimo/scrnaseq-pbmc-workflow:v1.0.4")
         #sp.add_argument("--image", default="scrnaseq-workflow")
         sp.add_argument("--snakefile", default="workflow/Snakefile")
         sp.add_argument("--configfile", default="config/config.yaml")
